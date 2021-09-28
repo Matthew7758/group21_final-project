945a00e5a08c7f47aef2cbcdf1cedc9afcf38989
God <Matthew7758@users.noreply.github.com>
2021-09-28 19:30:07 -0400

    Basic layout done.

---
+++ app/src/main/java/com/group21/android/mp/PlayActivity.kt
@@ -0,0 +1,11 @@
+package com.group21.android.mp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
