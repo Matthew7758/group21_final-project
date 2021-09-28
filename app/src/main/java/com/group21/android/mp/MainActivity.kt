e0eeb23eebb98d7cab7eecaebf84739d7b7c4103
Matthew <entei678@gmail.com>
2021-09-28 15:15:09 -0400

    First commit.

---
+++ app/src/main/java/com/group21/android/mp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.group21.android.mp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
