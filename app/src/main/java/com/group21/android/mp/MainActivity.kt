3303212325a75500acf095983f8308daa7a2feb0
God <Matthew7758@users.noreply.github.com>
2021-09-28 20:27:28 -0400

    Working on permissions.

--- app/src/main/java/com/group21/android/mp/MainActivity.kt
+++ app/src/main/java/com/group21/android/mp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.group21.android.mp
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var musicList: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_play)
+        setContentView(R.layout.activity_main)
+        musicList = this.findViewById(R.id.musicList)
+        checkPerms()
+        Toast.makeText(applicationContext,"Permissions granted!", LENGTH_SHORT).show()
+    }
+    private fun checkPerms() {
+        when {
+            ContextCompat.checkSelfPermission(applicationContext,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(applicationContext,Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED ->{
+                return
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                Toast.makeText(applicationContext,"Permissions required!",LENGTH_SHORT).show()
+            }
+            else -> {
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO), 69)
+            }
+        }
     }
 }
