cc8bb4621862dc1139c485159e9aca92e001a0e7
God <Matthew7758@users.noreply.github.com>
2021-09-29 21:50:20 -0400

    Download network service works.

--- app/src/main/java/com/group21/android/mp/MainActivity.kt
+++ app/src/main/java/com/group21/android/mp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.group21.android.mp
 
 import android.Manifest
-import android.content.ContentResolver
+import android.accounts.AccountManager.VISIBILITY_VISIBLE
+import android.app.DownloadManager
+import android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -12,11 +14,15 @@ import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.*
+import android.os.Handler
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.MimeTypeMap
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -24,11 +30,12 @@ import android.widget.Toast.LENGTH_SHORT
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.internal.ContextUtils.getActivity
 import com.google.gson.Gson
 import java.io.File
-import java.io.FileOutputStream
 import java.util.concurrent.TimeUnit
 
 
@@ -37,8 +44,8 @@ private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var songRecyclerView: RecyclerView
     private var adapter: SongAdapter? = SongAdapter(emptyList())
-    lateinit var resolver: ContentResolver
     private var songList: List<Song>? = null
+    private lateinit var downloadButton: Button
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,38 +54,67 @@ class MainActivity : AppCompatActivity() {
         songRecyclerView = this.findViewById(R.id.musicList) as RecyclerView
         songRecyclerView.layoutManager = LinearLayoutManager(applicationContext)
         songRecyclerView.adapter = adapter
+        downloadButton = this.findViewById(R.id.downloadButton)
 
+        downloadButton.setOnClickListener {
+            downloadFile()
+        }
+        Log.d(TAG,"${getExternalStoragePublicDirectory(DIRECTORY_MUSIC)}/test.mp3")
+        val file2 = File(Environment.getExternalStoragePublicDirectory(DIRECTORY_MUSIC),"test.mp3")
+        if(file2.exists()) {
+            Log.d(TAG,"test.mp3 EXISTS")
+            downloadButton.isClickable = false
+            downloadButton.isEnabled = false
+        }
         checkPerms()
         getAllAudioFromDevice(applicationContext)
         //Toast.makeText(applicationContext, "Permissions granted!", LENGTH_SHORT).show()
         val filename = "save.txt"
-        val file = File(applicationContext.filesDir,filename)
-        if(file.exists()) {
-            Log.d(TAG,"FILE EXISTS")
-            val fileContent = applicationContext.openFileInput(filename).bufferedReader().useLines {
-                lines ->
-                lines.fold("") {some, text ->
-                    "$some$text"
+        val file = File(applicationContext.filesDir, filename)
+        if (file.exists()) {
+            //Log.d(TAG, "FILE EXISTS")
+            val fileContent =
+                applicationContext.openFileInput(filename).bufferedReader().useLines { lines ->
+                    lines.fold("") { some, text ->
+                        "$some$text"
+                    }
                 }
-            }
-            Log.d(TAG, "FILE CONTENTS = $fileContent")
+            //Log.d(TAG, "FILE CONTENTS = $fileContent")
             //Toast.makeText(applicationContext,"FILE CONTENTS = $fileContent", LENGTH_SHORT).show()
             val songFile = File(fileContent)
-            if(songFile.exists()) {
+            if (songFile.exists()) {
                 //Toast.makeText(applicationContext,"SONG $fileContent FOUND!", LENGTH_SHORT).show()
-                Log.d(TAG, "SONG FILE $fileContent EXISTS")
+                //Log.d(TAG, "SONG FILE $fileContent EXISTS")
                 val intent = Intent(applicationContext, PlayActivity::class.java)
                 val gson: Gson = Gson()
                 val jsonString = gson.toJson(songList)
-                val song: Song? = songList!!.find {it.path == fileContent}
+                val song: Song? = songList!!.find { it.path == fileContent }
 
                 intent.putExtra("songList", jsonString)
                 intent.putExtra("songName", song!!.name)
-                intent.putExtra("position",songList!!.indexOf(song))
+                intent.putExtra("position", songList!!.indexOf(song))
                 startActivity(intent)
             }
         }
         displaySongs(songList)
+    }
+
+    private fun downloadFile() {
+        val uri = "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3".toUri()
+        val request: DownloadManager.Request = DownloadManager.Request(uri)
+        request.addRequestHeader("Accept", "application/pdf");
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_MUSIC, "test.mp3");
+        request.setNotificationVisibility(VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        val downloadManager: DownloadManager = applicationContext.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        downloadManager.enqueue(request)
+        Toast.makeText(applicationContext,"DOWNLOAD FILE CALLED", LENGTH_SHORT).show()
+        downloadButton.isEnabled = false
+        downloadButton.isClickable = false
+
+        Handler().postDelayed(this::displayDownloadMessage, 2000)
+    }
+    private fun displayDownloadMessage() {
+        Toast.makeText(applicationContext,"Please restart device to allow MediaStore to refresh.", LENGTH_SHORT).show()
     }
 
     private fun displaySongs(songList: List<Song>?) {
@@ -123,7 +159,7 @@ class MainActivity : AppCompatActivity() {
 
             intent.putExtra("songList", jsonString)
             intent.putExtra("songName", song.name)
-            intent.putExtra("position",songList!!.indexOf(song))
+            intent.putExtra("position", songList!!.indexOf(song))
             startActivity(intent)
         }
     }
@@ -188,7 +224,7 @@ class MainActivity : AppCompatActivity() {
             while (cursor.moveToNext()) {
                 val audioModel = Song()
                 val path: String = cursor.getString(0)
-                Log.d(TAG, path)
+                //Log.d(TAG, path)
                 val name: String = cursor.getString(1)
                 val album: String = cursor.getString(2)
                 val artist: String = cursor.getString(3)
@@ -198,10 +234,10 @@ class MainActivity : AppCompatActivity() {
                 audioModel.artist = artist
                 audioModel.path = path
                 audioModel.duration = duration
-                Log.d("$TAG Name :$name", " Album :$album")
-                Log.d("$TAG Path :$path", " Artist :$artist")
-                Log.d(TAG, duration)
-                Log.d(TAG, path)
+                //Log.d("$TAG Name :$name", " Album :$album")
+                //Log.d("$TAG Path :$path", " Artist :$artist")
+                //Log.d(TAG, duration)
+                //Log.d(TAG, path)
                 tempAudioList.add(audioModel)
             }
             cursor.close()
@@ -222,6 +258,12 @@ class MainActivity : AppCompatActivity() {
             ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
                 applicationContext,
                 Manifest.permission.MANAGE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.INTERNET
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
             ) == PackageManager.PERMISSION_GRANTED -> {
                 return
             }
@@ -234,7 +276,8 @@ class MainActivity : AppCompatActivity() {
                         Manifest.permission.READ_EXTERNAL_STORAGE,
                         Manifest.permission.RECORD_AUDIO,
                         Manifest.permission.MANAGE_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.INTERNET,
                     ), 69
                 )
             }
